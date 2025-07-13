60b4ec81630c887f30e5917a8341ef5d7ec19534
praveensinghal2010 <139489493+praveensinghal2010@users.noreply.github.com>
2025-07-13 12:40:42 +0530

    Update Printingtest.java

--- src/test/java/temp1/Printingtest.java
+++ src/test/java/temp1/Printingtest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Printing {
+public class Printingtest {
 
 	public static void main(String args[])
 	{
